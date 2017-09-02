5571046b9aca4746044ed22caaf100e2f2209a0b
SriManjuRaghavan <31558936+SriManjuRaghavan@users.noreply.github.com>
2017-09-02 14:24:33 +0530

    Create 12.c

---
+++ 12.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int number,new_number=0,remainder;
+    scanf("%d%d",&number);
+     if(number>=0&&number>=9)
+     {
+         printf("the given number is palindrome");
+     }
+     else if(number>0)
+     {
+         while(number!=0)
+         {
+           remainder=number%10;
+           number=number/10;
+           new_number=new_number*10+remainder;  
+         }
+         if(new_number==number)
+         {
+             printf("the entered number is palindrome");
+         }
+         else
+            printf("the entered number is not palindrome"))
+     }
+   else
+    printf("the entered option is invalid option");
+}
