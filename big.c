bf992024545d5dad11325ba85d2e17f8cc17e81d
SriManjuRaghavan <31558936+SriManjuRaghavan@users.noreply.github.com>
2017-09-02 12:42:16 +0530

    Update big.c

--- big.c
+++ big.c
@@ -1,16 +1,16 @@
 void main()
 {
-    
+
     int a,b,c;
     scanf("%d%d%d",&a,&b,&c);
     if(a>b&&a>c)
     {
-        printf("a is greater");
+        printf("first number is greater");
     }
     else if(b>a&&b>c)
-        printf("b is greater");
+        printf("second number is greater");
     elseif(c>a&&c>b)
-        printf("c is greater");
+        printf("third number is greater");
 else
 printf("invalid option you have entered");
 }
