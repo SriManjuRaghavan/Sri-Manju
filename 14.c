fe80d10f86a2af6ce8e48e663d2acde9502a3910
SriManjuRaghavan <31558936+SriManjuRaghavan@users.noreply.github.com>
2017-09-02 14:35:04 +0530

    Create 14.c

---
+++ 14.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int number1,number2i;
+    scanf("%d%d",&number1,number2);
+     if(number1>0&&number2>0)
+     {
+         for(i=number1;i<number2;i++)
+        if(i%3==0)
+         {
+             printf("%d",i)
+         }
+         
+     }
+   else
+    printf("the entered option is invalid option");
+}
