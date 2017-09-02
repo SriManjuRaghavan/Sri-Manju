7ee784305df8477cee493d80f3025fc2b9c11aac
SriManjuRaghavan <31558936+SriManjuRaghavan@users.noreply.github.com>
2017-09-02 11:44:53 +0530

    Create big.c

---
+++ big.c
@@ -0,0 +1,16 @@
+void main()
+{
+    
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b&&a>c)
+    {
+        printf("a is greater");
+    }
+    else if(b>a&&b>c)
+        printf("b is greater");
+    elseif(c>a&&c>b)
+        printf("c is greater");
+else
+printf("invalid option you have entered");
+}
