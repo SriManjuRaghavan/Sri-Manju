fac4fafcf3d6b363303eb2ae4e5af6bcc97cc5ad
SriManjuRaghavan <31558936+SriManjuRaghavan@users.noreply.github.com>
2017-09-02 12:29:56 +0530

    Update and rename sum.c to 9.c

--- 9.c
+++ 9.c
@@ -4,8 +4,14 @@ void main()
 {
     int number,sum=0
     scanf("%d",&number);
+    if(number>0)
+    {
     for(i=1;i<number;i++)
     {
         sum=sum+i;
     }
-    print("the sum of number is %d",sum);}
+    print("the sum of number is %d",sum);
+    }
+    else
+        printf("the entered number is invalid option");
+}
